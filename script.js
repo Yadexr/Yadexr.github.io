70978d697d6469a9b366340d29dec7826f5d2cf2
Yadexr <166477222+Yadexr@users.noreply.github.com>
2024-04-09 23:25:33 -0600

    Initial commit

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ document.getElementById("subjects-btn").addEventListener("click", function() {
 document.getElementById("help-btn").addEventListener("click", function() {
     window.location.href = "chatgpt.html";
 });
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("courses-btn").addEventListener("click", function() {
+        window.location.href = "cursos.html"; // Reemplaza "cursos.html" por la ruta de tu página de cursos
+    });
+
+    // Función para regresar a la página index.html
+    document.getElementById("back-btn").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+});
